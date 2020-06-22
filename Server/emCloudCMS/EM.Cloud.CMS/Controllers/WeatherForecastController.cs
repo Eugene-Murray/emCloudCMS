17cb40479084631fb56302c339f60de4ff1fc132
Eugene Murray <eugene.murray@outlook.com>
2020-06-22 22:19:42 +0100

    add auth

---
+++ Server/emCloudCMS/EM.Cloud.CMS/Controllers/WeatherForecastController.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using EM.Cloud.CMS.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+// https://stackoverflow.com/questions/30701006/how-to-get-the-current-logged-in-user-id-in-asp-net-core
+
+namespace EM.Cloud.CMS.Controllers
+{
+    [Authorize]
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly ILogger<WeatherForecastController> _logger;
+        private ApplicationUser _currentUser;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger, UserManager<ApplicationUser> userManager)
+        {
+            _logger = logger;
+            _userManager = userManager;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<WeatherForecast>> GetAsync()
+        {
+
+            GetCurrentUser().Wait();
+            var x = _currentUser;
+
+
+            var userId = User.FindFirst(ClaimTypes.NameIdentifier); // will give the user's userId
+            var userName = User.FindFirst(ClaimTypes.Email); // will give the user's userName
+
+            ApplicationUser applicationUser = await _userManager.GetUserAsync(User);
+            string userEmail = applicationUser?.Email; // will give the user's Email
+
+
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+
+        private async Task GetCurrentUser()
+        {
+            _currentUser = await _userManager.GetUserAsync(HttpContext.User);
+        }
+    }
+}
---
+++ Server/emCloudCMS/EM.Cloud.Data.EF/AuthContext.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EM.Cloud.Data.EF
+{
+    class AuthContext
+    {
+    }
+}
